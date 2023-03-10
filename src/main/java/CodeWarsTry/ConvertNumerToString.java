b092fdd86f295a2fd214e07397df1f9f62259784
Robin Schatzl Unix <rsjs1507@gmail.com>
2023-03-10 23:02:20 +0100

    N/A

--- src/main/java/CodeWarsTry/ConvertNumerToString.java
+++ src/main/java/CodeWarsTry/ConvertNumerToString.java
@@ -1,4 +1,43 @@
 package CodeWarsTry;
 
+
+
 public class ConvertNumerToString {
+
+    public static void main(String[] args) {
+
+
+        System.out.println(text);               //Hier wird der neue String "text" als String in die Konsole ausgegeben
+
+        System.out.println(text2);                 //und hier wird dann der neue String "text2" als String in die Konsole ausgegeben
+
+        System.out.println(numberToString(815));     //Hier wird der "numberToString" in der Konsole ausgegeben
+    }
+
+    public static String text = Integer.toString(42);  //Hier wird direkt ein int in einen String umgewandelt
+
+
+    static int number = 666;                       //Hier wird erst ein integer mit der bezeichnung "number" erstellt
+    public static String text2 = Integer.toString(number);  //wie oben wird dann der wert von "number" in einen String ausgegeben
+
+
+//_______________________________________________________________________________________________________________________________________
+//_________Die vorgaben oben wollte CodeWars so nicht da CodeWars public static String numberToString(int num) im Code haben wollte______
+    public static String numberToString(int num) {   //Hier wird eine String mit dem namen "numberToString" aufgerufen die in (die variable "int num") enthält
+        return Integer.toString(num); //hier wird der Integer dann in einen String vom wert"num" ausgegeben
+    }
+
+//_________Alternative lösungen auf CodeWars____________
+
+    public static String numberToString2(int num) { //Identisch zu meiner nur,
+        return String.valueOf(num);                 //anstelle von .toString wird .valueOf genommen
+    }
+
+    public static String numberToString(int num) { //Richtig coole version
+        return ""+num;          //"" steht vermutlich für String und +num gibt an was in einen String verwandelt werden soll
+    }
+
+
+
+
 }
