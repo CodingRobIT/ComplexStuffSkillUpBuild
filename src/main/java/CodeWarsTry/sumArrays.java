85f636199f73da786f8ee249cef8ad67cb25f850
robin <tmai02476@gmail.com>
2023-09-13 21:00:59 +0200

    added sum Arrays from CodeWars

---
+++ src/main/java/CodeWarsTry/sumArrays.java
@@ -0,0 +1,28 @@
+package CodeWarsTry;
+
+public class sumArrays {
+
+    public static double sum(double[] numbers) {
+        double sum = 0.0; //sum wird hier initialisiert mit dem wert 0.0
+
+        // hier wird int i als 0 Initialisiert und mit der länge der nummer "numbers" wird
+        // durch i++ der wert um 1 erhöt. Dadurch wird durch die zahlen (das Array double[]) durch iteriert
+        for (int i = 0; i < numbers.length; i++) {
+            sum += numbers[i]; // hier wird dann zu sum je die neue nummer "numbers" addiert
+        }
+        // Alternativ zu der for schleife oben
+//        for (double number : numbers) {
+//            sum += number;
+//        }
+
+        return sum; //hier wird dann, wenn die for Schleife fertig ist die gesamt summe "sum" ausgegeben
+    }
+
+    // Für CodeWars nicht relevant ist nur zum Darstellen
+    public static void main(String[] args) {
+
+        double[] numbers = {1.5, 2.0, 3.2, 0.84674};
+        double result = sum(numbers);
+        System.out.println("Die Summe der Zahlen ist: " + result);
+    }
+}
