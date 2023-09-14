74ff73cd9296376c3b276e878f8c3f44a40619b9
robin <tmai02476@gmail.com>
2023-09-14 20:26:53 +0200

    added CodeWars Test Jenny's secret message.

---
+++ src/main/java/CodeWarsTry/JennysSecretMessage.java
@@ -0,0 +1,17 @@
+package CodeWarsTry;
+
+public class JennysSecretMessage {
+    public static String greet(String name) {
+        if(name.equals("Johnny")){
+            return "Hello, my love!";
+        }
+        else {
+            return String.format("Hello, %s!", name);
+        }
+    }
+
+    public static void main(String[] args) {
+        String name = "Johnny";
+        System.out.printf(greet(name));
+    }
+}
